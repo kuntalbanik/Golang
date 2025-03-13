d3d0f62f556cc3d912cb09cfc8d735a8a499ca5e
kuntalbanik <deisiduke1986@gmail.com>
2025-03-13 18:04:24 +0530

    Update function

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
-	"mylearning/myinput"
+	"mylearning/function"
+	// "mylearning/myinput"
 )
 
 func main() {
 	fmt.Println("Hello")
-	myinput.CustomInput()
+	// myinput.CustomInput()
 
 	var version = "1.0"
 	fmt.Println(version)
+
+	addition := function.Add(10, 20)
+	fmt.Println("Addition of two numbers :", addition)
+
 }
